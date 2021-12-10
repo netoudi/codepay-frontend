fd99bec1db31de1e7607e048c6734648137690e2
netoudi <netoudi@rocketmail.com>
2021-12-09 23:33:33 -0300

    Add @mui/material

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import * as React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+import createEmotionCache from 'utils/createEmotionCache';
+
+import theme from 'styles/theme';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp;
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>CodePay</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
